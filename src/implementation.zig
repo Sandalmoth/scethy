d58e6878ccfd69b3e2332d8122743a310d28852a
Jonathan <sandalmoth@pm.me>
2024-08-21 19:40:05 +0200

    more properly allocate implementations

--- src/implementation.zig
+++ src/implementation.zig
@@ -12,11 +12,42 @@ const PageIndex = struct {
 
 const PageHeader = struct {
     offset: usize,
+    end: usize,
+};
+
+const ImplHeader = struct {
+    size: u32,
+    _align: u32,
 };
 
 const Page = struct {
     head: PageHeader,
     bytes: [Pool.BLOCK_SIZE - 64]u8,
+
+    fn create(pool: *Pool) *Page {
+        const page = pool.create(Page);
+        page.head = .{
+            .offset = @intFromPtr(&page.bytes[0]) + 8,
+            .end = @intFromPtr(&page.bytes[0]) + page.bytes.len,
+        };
+        return page;
+    }
+
+    fn alloc(page: *Page, comptime Impl: type) ?*Impl {
+        const result: *Impl = @ptrFromInt(page.head.offset);
+        page.head.offset = std.mem.alignForward(usize, page.head.offset, @alignOf(Impl));
+        page.head.offset += @sizeOf(Impl);
+        if (page.head.offset > page.head.end) return null;
+        const header = getHeader(result);
+        header.size = @sizeOf(Impl);
+        header._align = @alignOf(Impl);
+        page.head.offset += 8;
+        return result;
+    }
+
+    fn getHeader(ptr: *anyopaque) *ImplHeader {
+        return @ptrFromInt(@intFromPtr(ptr) - 8);
+    }
 };
 
 pub const ImplStorage = struct {
@@ -48,9 +79,20 @@ pub const ImplStorage = struct {
         comptime Impl: type,
         val: Impl,
     ) Interface {
-        // TODO allocate properly
-        const impl = storage.pool.create(Impl);
+        if (storage.n_pages == 0) storage.newPage();
+        const impl = storage.page_index.pages[storage.n_pages - 1].?.alloc(Impl) orelse blk: {
+            storage.newPage();
+            break :blk storage.page_index.pages[storage.n_pages - 1].?.alloc(Impl) orelse @panic(
+                "Page.alloc - " ++ @typeName(Impl) ++ " cannot fit with the given block size",
+            );
+        };
         impl.* = val;
         return impl.interface();
+    }
+
+    fn newPage(storage: *ImplStorage) void {
+        if (storage.n_pages == INDEX_SIZE) @panic("Could not create a new page, index is full");
+        storage.page_index.pages[storage.n_pages] = Page.create(storage.pool);
+        storage.n_pages += 1;
     }
 };
