7ddf053893919af36f5325ff045526fdd9a05484
Jonathan <sandalmoth@pm.me>
2024-08-24 00:36:52 +0200

    make the decls public...

--- src/root.zig
+++ src/root.zig
@@ -1,7 +1,7 @@
-const Entity = @import("table.zig").Entity;
-const nil = @import("table.zig").nil;
-const Pool = @import("table.zig").Pool;
-const Table = @import("table.zig").Table;
+pub const Entity = @import("table.zig").Entity;
+pub const nil = @import("table.zig").nil;
+pub const Pool = @import("table.zig").Pool;
+pub const Table = @import("table.zig").Table;
 
 // so that we can run the tests on this file and all is used
 // (does this have any other side-effects?)
